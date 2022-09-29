06576e4f4a2907c6c999b7697d85fc170ab46fb5
Karim Mousa <karimmousa202@gmail.comgit config --global --replace-all user.email karimmousa202@gmail.com>
2022-09-29 12:04:04 +0100

    sui

--- primes.py
+++ primes.py
@@ -2,5 +2,23 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <1:
+        raise ValueError
+        
     list = []
+    num = 2
+
+    while len(list) <= number_of_primes:
+
+      prime = True
+      #prime checker
+      for x in range (2,num):
+        if num % x == 0:
+          prime = False
+
+      if prime:
+        list.append(num)
+
+      num +=1
+
     return list
